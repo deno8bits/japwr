46338be3f5dff67758808dc6d5925cc928c6ecb9
deno8bits <denobyte2@gmail.com>
2023-05-30 11:17:30 +0200

    Started on multireddit support Also started on auth (unsure if it works)

--- japwr/auth.py
+++ japwr/auth.py
@@ -5,5 +5,12 @@ import json
 # TODO: This
 # Implement authentication stuff
 class Auth:
-    def __init__(self) -> None:
-        raise Exception('Not Done Yet')
+    def __init__(self, clientID: str, clientSecret: str) -> None:
+        self.clientID = clientID
+        self.clinetSecret = clientSecret
+
+    def getParams(self) -> dict[str]:
+        return {
+            'client_id': self.clientID,
+            'client_secret': self.clinetSecret
+        }
--- japwr/classes.py
+++ japwr/classes.py
@@ -1,5 +1,12 @@
 from japwr.utils import ConnectionHandler
-import japwr.error
+"""
+TODO: Implement a better way of handling feeds (Subreddit, Multireddit etc.)
+ref:
+    https://www.reddit.com/dev/api#section_listings
+
+Multireddits also use /new /hot etc for its stuff so a parent class might be a good idea for handling feeds
+to avoid repeating alot of code
+"""
 
 
 class Post:
@@ -45,6 +52,33 @@ class Subreddit:
 
 
 class MultiReddit:
+    # TODO: Figure out how to actually interact with multireddits using the api
+    # Either the API docs for this is unclear or I'm an idiot
+    # refs:
+    #   https://www.reddit.com/dev/api#section_multis
+    #   https://www.reddit.com/dev/api#section_listings
+    #
+    # 2023/05/30 10:38:
+    # multipath = /user/<username>/m/<name>
+    # /top /hot /best etc. don't work at the end it just returns 404 saying the feed was not found
+    # Will try with clientID and clientSecret might just be wierd
+    #
+    # 2023/05/30 10:42: ^ no
+    # I don't know how in the absolute hell your supposed to get posts.
+    #
+    # 2023/05/30 11:12: There are no words to describe the absolute fury I'm feeling
+    # According to the documentation the url for multis are https://api.reddit.com/api/multi/<multipath>
+    # BUT no where in the documentation does it mention the place where you get the listing (posts) from the api
+    #
+    # So I went digging in PRAWs code which did not really give a solution but it put me on the correct path
+    #
+    # The url for gettings posts from the api for multireddits is https://api.reddit.com/<multipath>
+    #
+    # I don't know why they did it like this nor do I know why its not in the documention but here it is.
+    #
+    # This took way longer then it should have and I'm never getting back the ~30 mins I spent looking for
+    # this damned url
+    #
     """
     Creates a MultiReddit Instance
     """
@@ -52,6 +86,8 @@ class MultiReddit:
         self.username = username
         self.feedName = feedName
         self.connHandler = connHandler
+        self.urlBase = f'https://api.reddit.com/user/{username}/m/{feedName}/'
+        raise Exception('Not Implemented Yet')
 
     def new(self, limit: int = 25) -> dict:
 
