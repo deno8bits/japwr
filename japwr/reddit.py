81ba32c2a8fa148e4fe2dde7a5318d29b2d0d630
deno8bits <denobyte2@gmail.com>
2023-05-30 11:17:30 +0200

    Started on multireddit support Also started on auth (unsure if it works)

--- japwr/reddit.py
+++ japwr/reddit.py
@@ -1,11 +1,18 @@
 from japwr.utils import ConnectionHandler
 from japwr.classes import Subreddit, MultiReddit
+from japwr.auth import Auth
 
 
 class Reddit:
-    def __init__(self, userAgent: str) -> None:
+    def __init__(self, userAgent: str, clientID: str = '', clientSecret: str = '') -> None:
+        headers = {'User-Agent': userAgent}
+        params = {}
+        if clientID and clientSecret:
+            auth = Auth(clientID, clientSecret)
+            params = params | auth.getParams
         self.userAgent: str = userAgent
-        self.connHandler = ConnectionHandler({'User-Agent': self.userAgent})
+
+        self.connHandler = ConnectionHandler(headers, params)
 
     def subreddit(self, name) -> Subreddit:
         sub = Subreddit(self.connHandler, name)
