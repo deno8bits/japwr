c0951bc4fa9fdc60776ca238b0d1fe42f599e708
Deno8bits <Denobyte2@gmail.com>
2023-05-31 08:46:33 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='japwr',
+    version='0.0.0',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.10"',
+    ],
+)
