048fe9c2cac31ae3cfd91b23bb0878cbbc41051d
deno8bits <denobyte2@gmail.com>
2023-06-21 11:01:10 +0200

    Updated Docs and README Updated docs for ConnectionHandler Updated README

--- japwr/utils.py
+++ japwr/utils.py
@@ -16,13 +16,30 @@ class ConnectionHandler:
 
     """
     def __init__(self, headers: dict, params: dict = {}) -> None:
+        """Initializes a ConnectionHandler
+
+        Args:
+            headers (dict): Global Headers to add, Required for the UserAgent
+            params (dict, optional): Global Params to add (eg. clientID and clientSecret). Defaults to {}.
+        """
         self.headers = {
             'Accpets': 'application/json'
-        } | headers
+        } | headers  # Adds to the global headers, required to add UserAgent
 
         self.params = {} | params  # Used to specify global params eg. clientID and clientSecret for later
 
     def get(self, url: str, **kwargs) -> dict:
+        """Sends a get request to the specified url and uses the global headers and params
+
+        Use kwargs to add extra params
+
+        Args:
+            url (str): URL to send request to
+            **kwargs (): Extra params to send with the global ones
+
+        Returns:
+            dict: Returns the parsed response JSON
+        """
 
         params = self.params | kwargs
 
@@ -35,9 +52,32 @@ class ConnectionHandler:
         return req
 
     def post(self, url: str, *kwargs) -> dict:
+        """Not Implemented Yet
+
+        Will be used to change stuff
+
+        Args:
+            url (str): _description_
+
+        Raises:
+            Exception: _description_
+
+        Returns:
+            dict: _description_
+        """
         raise Exception('Not Implemented Yet')
 
     def checkError(self, res: Response) -> None:
+        """_summary_
+
+        Args:
+            res (Response): Response to check if it errored out.
+
+        Raises:
+            error.Unauthorized: Got a 403
+            error.RateLimited: Was Ratelimited
+            Exception: Got a error that was not implemented by the wrapper yet.
+        """
         if res.status_code != 200:
             match res.status_code:
                 case 403:
